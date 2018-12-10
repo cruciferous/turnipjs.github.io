84ebf985ea64b0932e18b6fe6e117de0ae60b4bc
Charlie Moonen <charliemoonen@gmail.com>
2018-12-10 16:43:40 -0600

    Create painter.py

---
+++ painter.py
@@ -0,0 +1,13 @@
+import time
+start = time.time()
+
+d = pow(2,19)
+c = 0
+for a in range(0,d):
+  b = bin(a)[2:]
+  b = (19-len(b))*'0' + b
+  if ('000' in b or '11' in b):
+    c = c+1
+print(d-c)
+
+print("--- %s seconds ---" % (time.time() - start))
